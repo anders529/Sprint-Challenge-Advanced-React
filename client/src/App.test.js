3d62c0162e2d0bfbbbaa1b978f458460fc1ab9c1
Aaron Andersen <anderseana@tutanota.com>
2020-01-17 14:10:51 -0600

    fixed my tests js file

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { findAllByText } from '@testing-library/react';
+import { findByText, render } from '@testing-library/react';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
